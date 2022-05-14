13d6dc462c82a8e8ac18630c0e9804faa497e402
Lucas Garcez <lucasgarcezdev@gmail.com>
2022-05-14 20:35:41 -0300

    EditModal on UserList

--- src/components/UserList/UserList.tsx
+++ src/components/UserList/UserList.tsx
@@ -1,14 +1,13 @@
+import { useState } from "react";
 import { useQuery } from "react-query";
 import { api } from "../../api/api";
 import { User } from "../../models/UserModel";
+import { EditModal } from "../EditModal/EditModal";
 import { UserCard } from "../UserCard/UserCard";
 import { Container } from "./styles";
 export function UserList() {
+  const [selectedUser, setSelectedUser] = useState<User | null>(null);
   const { data, isError, isLoading } = useQuery("user-list", api.getUsers);
-
-  function onClickEdit() {
-    //
-  }
 
   return (
     <Container>
@@ -16,8 +15,15 @@ export function UserList() {
       {isLoading && <h3>Carregando...</h3>}
       {isError && <h3>Ocorreu algum problema :(</h3>}
       {data?.map((user) => (
-        <UserCard user={user} onClickEdit={onClickEdit} />
+        <UserCard user={user} onClickEdit={() => setSelectedUser(user)} />
       ))}
+      {selectedUser && (
+        <EditModal
+          user={selectedUser}
+          show={!!selectedUser}
+          handleClose={() => setSelectedUser(null)}
+        />
+      )}
     </Container>
   );
 }
