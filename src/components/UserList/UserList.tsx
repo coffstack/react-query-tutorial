ccf028aadcdddc0386d322042a5fccfbbc89a2c1
Lucas Garcez <lucasgarcezdev@gmail.com>
2022-05-14 14:58:46 -0300

    useQuery

--- src/components/UserList/UserList.tsx
+++ src/components/UserList/UserList.tsx
@@ -1,28 +1,10 @@
-import { useEffect, useState } from "react";
+import { useQuery } from "react-query";
 import { api } from "../../api/api";
 import { User } from "../../models/UserModel";
 import { UserCard } from "../UserCard/UserCard";
 import { Container } from "./styles";
 export function UserList() {
-  const [userList, setUserList] = useState<User[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
-
-  useEffect(() => {
-    fetchUsers();
-  }, []);
-
-  async function fetchUsers() {
-    try {
-      setIsLoading(true);
-      const users = await api.getUsers();
-      setUserList(users);
-    } catch (error) {
-      setIsError(true);
-    } finally {
-      setIsLoading(false);
-    }
-  }
+  const { data, isError, isLoading } = useQuery("user-list", api.getUsers);
 
   function onClickEdit() {
     //
@@ -33,7 +15,7 @@ export function UserList() {
       <h1>Lista de Usuários</h1>
       {isLoading && <h3>Carregando...</h3>}
       {isError && <h3>Ocorreu algum problema :(</h3>}
-      {userList.map((user) => (
+      {data?.map((user) => (
         <UserCard user={user} onClickEdit={onClickEdit} />
       ))}
     </Container>
