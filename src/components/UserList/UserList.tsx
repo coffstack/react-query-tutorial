582c444ed61039864cd749bdb25a94d7b3ed6d1c
Lucas Garcez <lucasgarcezdev@gmail.com>
2022-05-14 14:36:44 -0300

    fetchUsers on UserList

--- src/components/UserList/UserList.tsx
+++ src/components/UserList/UserList.tsx
@@ -1,5 +1,19 @@
+import { useEffect, useState } from "react";
+import { api } from "../../api/api";
+import { User } from "../../models/UserModel";
 import { Container } from "./styles";
 export function UserList() {
+  const [userList, setUserList] = useState<User[]>([]);
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  async function fetchUsers() {
+    const users = await api.getUsers();
+    setUserList(users);
+  }
+
   return (
     <Container>
       <h1>Lista de Usuários</h1>
